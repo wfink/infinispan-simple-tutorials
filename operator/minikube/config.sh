06cf47b5d29ac5e051aae88eeff7dd1a095897f4
Galder Zamarreño <galder@zamarreno.com>
2019-07-16 14:23:56 +0100

    Add Minikube config options

---
+++ operator/minikube/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+PROFILE=operator-simple-tutorial
+VMDRIVER=${2}
+
+minikube profile ${PROFILE}
+minikube config set memory 4096
+minikube config set cpus 4
+minikube config set disk-size 10GB
+
+if [[ -n "${VMDRIVER}" ]]; then
+    echo "Using VM driver '$VMDRIVER'"
+    # Set virtual machine drivers.
+    minikube config set vm-driver ${VMDRIVER}
+fi
